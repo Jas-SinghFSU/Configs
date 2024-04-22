305cf026c475c1e816ee4355c72877a1b2803cd7
Jas Singh <jaskiratpal.singh@outlook.com>
2024-04-21 23:31:10 -0700

    Added wezterm

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,19 @@
+local wezterm = require 'wezterm'
+
+return {
+  default_prog = { "/usr/bin/zsh" },
+  color_scheme = "Catppuccin Mocha",
+  tab_bar_at_bottom = true,
+  use_fancy_tab_bar = false,
+  hide_tab_bar_if_only_one_tab = true,
+  font = wezterm.font("JetBrainsMono Nerd Font Mono", { weight = 700 }),
+  font_size = 12,
+  enable_wayland = true,
+  warn_about_missing_glyphs = false,
+  cell_width = 0.9,
+  -- window_decorations = "RESIZE",
+  -- dpi = 110,
+  -- freetype_render_target = "Normal",
+  -- freetype_load_flags = "NO_AUTOHINT",
+  -- line_height = 0.9,
+}
