dc47e99e4190ca9207ee7b46d370cc960f5720f5
Jas Singh <jaskiratpal.singh@outlook.com>
2024-10-20 20:24:27 -0700

    Wezterm

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,22 +1,31 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 return {
-  default_prog = { "/usr/bin/zsh" },
-  color_scheme = "Catppuccin Mocha",
-  tab_bar_at_bottom = true,
-  use_fancy_tab_bar = false,
-  hide_tab_bar_if_only_one_tab = true,
-  colors = {
-    cursor_bg = "#c4ccff",
-  },
-  font = wezterm.font("JetBrainsMono Nerd Font Mono", { weight = 'Bold' }),
-  font_size = 12,
-  enable_wayland = true,
-  warn_about_missing_glyphs = false,
-  cell_width = 0.9,
-  -- window_decorations = "RESIZE",
-  -- dpi = 210,
-  freetype_load_target = "Light",
-  -- freetype_load_flags = "NO_AUTOHINT",
-  -- line_height = 0.9,
+	default_prog = { "/usr/bin/zsh" },
+	front_end = "WebGpu",
+	color_scheme = "Catppuccin Mocha",
+	tab_bar_at_bottom = true,
+	use_fancy_tab_bar = false,
+	hide_tab_bar_if_only_one_tab = true,
+	colors = {
+		cursor_bg = "#c4ccff",
+	},
+	font = wezterm.font("JetBrains Mono", { weight = "DemiBold" }),
+	font_size = 12,
+	enable_wayland = true,
+	warn_about_missing_glyphs = false,
+	window_padding = {
+		right = 4,
+		left = 4,
+		bottom = 0,
+	},
+	use_resize_increments = true,
+
+	-- allow_square_glyphs_to_overflow_width = "",
+	-- cell_width = 1,
+	-- window_decorations = "RESIZE",
+	-- dpi = 100,
+	-- freetype_load_target = "Light",
+	-- freetype_load_flags = "NO_AUTOHINT",
+	-- line_height = 0.9,
 }
